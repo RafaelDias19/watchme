cece691b2453dd93da46a25e41e08318cb07edc2
Rafael Dias <r_dias_b@hotmail.com>
2022-03-02 10:30:00 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar genres={genres} selectedGenreId={selectedGenreId} setSelectedGenreId={setSelectedGenreId} />
+      <Content selectedGenreId={selectedGenreId}/>
+    </div>
+  )
+}
